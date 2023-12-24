5f14db1f2fed3b50c2985b5497b172841c83a6ea
Marina Diubailo <marinadiubailo@gmail.com>
2023-12-24 11:17:55 +0300

    fix 25+5 clock

--- 25+5Clock/index.jsx
+++ 25+5Clock/index.jsx
@@ -4,13 +4,33 @@ import React, {
 } from 'https://cdn.skypack.dev/react@17.0.1';
 import ReactDOM from 'https://cdn.skypack.dev/react-dom@17.0.1';
 
+const breakLength = 5 * 60;
+const sessionLength = 25 * 60;
+
 const App = () => {
-    const [breakLength, setBreaklength] = useState(5);
-    const [sessionLength, setSessionlength] = useState(25);
+    const [breakView, setBreakView] = useState(breakLength / 60);
+    const [sessionView, setSessionView] = useState(sessionLength / 60);
 
     const resetHandler = () => {
         setBreaklength(5);
         setSessionlength(25);
+    };
+
+    const breakDecrementHandler = () => {
+        if (breakView === 1) return;
+        setBreakView((prev) => prev - 1);
+    };
+    const breakIncrementHandler = () => {
+        if (breakView === 60) return;
+        setBreakView((prev) => prev + 1);
+    };
+    const sessionDecrementHandler = () => {
+        if (sessionView === 1) return;
+        setSessionView((prev) => prev - 1);
+    };
+    const sessionIncrementHandler = () => {
+        if (sessionView === 60) return;
+        setSessionView((prev) => prev + 1);
     };
 
     return (
@@ -20,14 +40,20 @@ const App = () => {
                 <div className="container">
                     <h2 id="break-label">Break Length</h2>
                     <div className="arrows">
-                        <button id="break-decrement">
+                        <button
+                            id="break-decrement"
+                            onClick={breakDecrementHandler}
+                        >
                             <i
                                 class="fa fa-2x fa-arrow-down"
                                 aria-hidden="true"
                             ></i>
                         </button>
-                        <span id="break-length">{breakLength}</span>
-                        <button id="break-increment">
+                        <span id="break-length">{breakView}</span>
+                        <button
+                            id="break-increment"
+                            onClick={breakIncrementHandler}
+                        >
                             <i
                                 class="fa fa-2x fa-arrow-up"
                                 aria-hidden="true"
@@ -38,14 +64,20 @@ const App = () => {
                 <div className="container">
                     <h2 id="session-label">Session Length</h2>
                     <div className="arrows">
-                        <button id="session-decrement">
+                        <button
+                            id="session-decrement"
+                            onClick={sessionDecrementHandler}
+                        >
                             <i
                                 class="fa fa-2x fa-arrow-down"
                                 aria-hidden="true"
                             ></i>
                         </button>
-                        <span id="session-length">{sessionLength}</span>
-                        <button id="session-increment">
+                        <span id="session-length">{sessionView}</span>
+                        <button
+                            id="session-increment"
+                            onClick={sessionIncrementHandler}
+                        >
                             <i
                                 class="fa fa-2x fa-arrow-up"
                                 aria-hidden="true"
